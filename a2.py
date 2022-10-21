e834bbb8ce26d618dde7bc156e366f41dcd32e2c
Aye Pyae Phyo Kyaw <102339171+appk18@users.noreply.github.com>
2022-10-21 21:33:42 +0800

    Add files via upload

---
+++ a2.py
@@ -0,0 +1,14 @@
+a = 50
+
+print("Interger : ", a)
+# hexadecimal
+hex_a = hex(a)
+print("Hexadecimal : ",hex_a)
+
+## octal
+oct_a = oct(a)
+print("octal : ",oct_a)
+
+#binary
+bin_a = bin(a)
+print("binary : ",bin_a)
